de09f70c996f156e36284c0fcae5739fea022913
Sayantan karmakar <sayantank22@gmail.com>
2020-11-11 12:24:37 +0530

    finalizing deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/departments', departmentRoutes);
 app.use('/api/requests', requestRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
